924609cec93955e5fe5c88b6eca347c56e511da1
sharmavvek23 <77957895+sharmavvek23@users.noreply.github.com>
2021-06-17 22:29:33 +0530

    Update server-detail.sh

--- server-detail.sh
+++ server-detail.sh
@@ -5,4 +5,4 @@ Up_time=$(uptime | awk 'NR==1{print $3}')
 ipaddress=$(ifconfig | awk 'NR==18{print $2}')
 
 echo "s.no,server_name,OS_type,Up_time,ipaddress" >server-details.excel
-echo "1,$(uname -n),$(uname),$(uptime | awk 'NR==1{print $3}'),$(ifconfif | awk 'NR==18{print $2}')" >>server-details.excel
+echo "1,$(uname -n),$(uname),$(uptime | awk 'NR==1{print $3}'),$(ifconfig | awk 'NR==18{print $2}')" >>server-details.excel
