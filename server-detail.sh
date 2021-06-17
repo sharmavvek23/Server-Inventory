e86c9758151fdebf113dd3caa305f8669c93f914
sharmavvek23 <77957895+sharmavvek23@users.noreply.github.com>
2021-06-17 21:36:34 +0530

    Create server-detail.sh

---
+++ server-detail.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+server_name=$(uname -a)
+OS_type=$(uname)
+Up_time=$(uptime | awk 'NR==1{print $3}')
+ipaddress=$(ifconfif | awk 'NR==18{print $2}')
+
+echo "s.no,server_name,OS_type,Up_time,ipaddress" >server-details
+echo "1,$(uname -a),$(uname),$(uptime | awk 'NR==1{print $3}'),$(ifconfif | awk 'NR==18{print $2}')" >>server-details
